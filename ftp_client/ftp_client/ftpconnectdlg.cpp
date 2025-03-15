cc89ea98d10bdcfc2b97df3c9ed75da500ee5251
幺妹儿 <@LAPTOP-71VD80KT>
2025-03-15 11:02:38 +0800

    添加项目文件。

---
+++ ftp_client/ftp_client/ftpconnectdlg.cpp
@@ -0,0 +1,34 @@
+﻿#include "ftpconnectdlg.h"
+#include "ui_ftpconnectdlg.h"
+
+FtpConnectDlg::FtpConnectDlg(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::FtpConnectDlg)
+{
+    ui->setupUi(this);
+//    ui->edit_host->setText("127.0.0.1");//192.168.217.214
+    ui->edit_host->setText("192.168.217.214");//192.168.217.214
+    ui->edit_user->setText("xb1520");
+    ui->edit_pass->setText("xb1520");
+    connect(ui->btn_connect,&QPushButton::clicked,this,&FtpConnectDlg::onConnect);
+}
+
+FtpConnectDlg::~FtpConnectDlg()
+{
+    delete ui;
+}
+
+FTP_DATA FtpConnectDlg::ftp_data()
+{
+    return
+    {
+        .host = ui->edit_host->text()
+        ,.user = ui->edit_user->text()
+        ,.pass = ui->edit_pass->text()
+    };
+}
+
+void FtpConnectDlg::onConnect()
+{
+    QDialog::accept();
+}
---
+++ ftp_client/ftp_client/mainwindow.h
@@ -0,0 +1,51 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include "ftp.h"
+
+#include <QLabel>
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+private:
+    void init_menu();
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+public:
+    void timerEvent(QTimerEvent *e);
+    void closeEvent(QCloseEvent* e);
+public slots:
+    void onConnect();
+    void cellDoubleClicked(int row, int idx);
+    void cdupAction();
+    void refAction();
+    void putAction();
+    void getAction();
+    void delAction();
+
+public:
+    static QString cur_time();
+private:
+    QString get_file_name(int row);
+    void set_pwd();
+    void clear_ui_list();
+    void insert_list(const std::vector<FTP_FILE_INFO>& list);
+    void insert_row(int row,const FTP_FILE_INFO& info);
+    void insert_item(int row,int idx,QString item);
+private:
+    Ui::MainWindow *ui;
+    QLabel ui_connect_status;
+    QLabel ui_pwd;
+    QLabel ui_cur_time;
+    int timer1;
+    Ftp ftp;
+};
+#endif // MAINWINDOW_H
